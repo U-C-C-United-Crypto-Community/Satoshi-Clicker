de34d415142d878c5fe1da8e37c167271641c887
Sono Willbrand <sono00@web.de>
2021-06-21 12:01:40 +0200

    added AtomicAsset Api

--- scripts/wax.js
+++ scripts/wax.js
@@ -1,11 +1,19 @@
+const { ExplorerApi } = require("atomicassets");
+const fetch = require("node-fetch");
+const api = new ExplorerApi(
+  "https://test.wax.api.atomicassets.io",
+  "atomicassets",
+  { fetch }
+);
+
+console.log(api);
 //normal login. Triggers a popup for non-whitelisted dapps
 const wax = new waxjs.WaxJS("https://wax.greymass.com", null, null, false);
 
 //normal login. Triggers a popup for non-whitelisted dapps
 async function login() {
   try {
-    const userAccount = await wax.login();
-    console.log(userAccount);
+    userAccount = await wax.login();
   } catch (e) {
     console.log(e);
   }
@@ -13,5 +21,16 @@ async function login() {
 login();
 
 async function buyItem(item) {
-  console.log(item);
+  const actions = (await api.action).mintasset(
+    [{ actor: userAccount, permission: "active" }],
+    "collection",
+    "scheme",
+    -1,
+    "pinknetworkx",
+    { name: "test" },
+    { species: "test2" }
+  );
+  console.log(actions);
 }
+
+// create the action to mint an asset
