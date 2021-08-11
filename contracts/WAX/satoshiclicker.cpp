98a4d5e9a606d4b87c65c0e8897ac96db601305d
Sono Willbrand <sono00@web.de>
2021-08-11 10:51:16 +0200

    commented contract

--- contracts/WAX/satoshiclicker.cpp
+++ contracts/WAX/satoshiclicker.cpp
@@ -1,15 +1,25 @@
 #include <satoshiclicker.hpp>
-
+/** 
+ * Calls the atomicassets mintasset action, as this contract is authorized to mint NFTs.
+ * 
+ * @required_auth: new_asset_owner account 
+*/
 ACTION satoshiclicker::mintasset(name collection_name, name schema_name,
                                  int32_t template_id, name new_asset_owner, ATTRIBUTE_MAP mutable_data, string amount, string memo, string hash)
 {
-    check(getFreezeFlag().frozen == 0, "Contract is frozen!");
-    validate(new_asset_owner, memo, hash, amount);
     require_auth(new_asset_owner);
+
+    // check is required to avoid string length overflow
+    check(memo.length() == 12 && amount.length() < 150 && name{new_asset_owner}.to_string().length() < 15, "Invalid memo!");
+
+    check(getFreezeFlag().frozen == 0, "Contract is frozen!");
     check(schema_name != "invfriend"_n, "Invalid call!");
-    check(black_list.find(new_asset_owner.value) == black_list.end(),
-          "User is banned!");
+    check(black_list.find(new_asset_owner.value) == black_list.end(), "User is banned!");
+
+    validate(new_asset_owner, memo, hash, amount);
+
     ATTRIBUTE_MAP immutable_data;
+
     action(permission_level{
                get_self(), "active"_n},
            "atomicassets"_n, "mintasset"_n, std::make_tuple(get_self(), collection_name, schema_name, template_id, new_asset_owner, immutable_data, mutable_data, NULL))
@@ -17,10 +27,15 @@ ACTION satoshiclicker::mintasset(name collection_name, name schema_name,
     updatebtc(new_asset_owner, amount);
 }
 
+/**
+ * Mints the special NFT for the referee and the receiver.
+ * 
+ * @required_auth: the receiver account  
+*/
 ACTION satoshiclicker::mintrefasset(name collection_name, name schema_name, int32_t template_id, name ref, name receiver)
 {
-    check(getFreezeFlag().frozen == 0, "Contract is frozen!");
     require_auth(receiver);
+    check(getFreezeFlag().frozen == 0, "Contract is frozen!");
     check(black_list.find(ref.value) == black_list.end() && black_list.find(receiver.value) == black_list.end(), "User is banned!");
     check(schema_name == "invfriend"_n && ref_list.find(receiver.value) == ref_list.end(),
           "Receiver already has another friend :(");
@@ -38,19 +53,32 @@ ACTION satoshiclicker::mintrefasset(name collection_name, name schema_name, int3
                      { u.user = receiver; });
 }
 
+/**
+ * Upgrades an asset with new_mutable_data.
+ * 
+ * @required_auth: the asset_owner account
+*/
 ACTION satoshiclicker::upgrade(name asset_owner, uint64_t asset_id, ATTRIBUTE_MAP new_mutable_data, string amount, string memo, string hash)
 {
-    check(getFreezeFlag().frozen == 0, "Contract is frozen!");
-    validate(asset_owner, memo, hash, amount);
     require_auth(asset_owner);
+
+    // check is required to avoid string length overflow
+    check(memo.length() == 12 && amount.length() < 150 && name{asset_owner}.to_string().length() < 15, "Invalid memo!");
+    check(getFreezeFlag().frozen == 0, "Contract is frozen!");
     check(black_list.find(asset_owner.value) == black_list.end(), "User is banned!");
+    validate(asset_owner, memo, hash, amount);
+
     action(permission_level{
                get_self(), "active"_n},
            "atomicassets"_n, "setassetdata"_n, std::make_tuple(get_self(), asset_owner, asset_id, new_mutable_data))
         .send();
     updatebtc(asset_owner, amount);
 }
-
+/**
+ * Bans an account.
+ * 
+ * @required_auth: the contract itself
+*/
 ACTION satoshiclicker::ban(name user)
 {
     require_auth(get_self());
@@ -60,6 +88,11 @@ ACTION satoshiclicker::ban(name user)
                        { u.user = user; });
 }
 
+/**
+ * Unbans an account.
+ * 
+ * @required_auth: the contract itself 
+*/
 ACTION satoshiclicker::unban(name user)
 {
     require_auth(get_self());
@@ -68,18 +101,31 @@ ACTION satoshiclicker::unban(name user)
     black_list.erase(itr);
 }
 
+/**
+ * Freezes the smart contract so no action are available.
+ * 
+ * @required_auth: the contract itself
+*/
 ACTION satoshiclicker::freeze()
 {
     require_auth(_self);
     setFreezeFlag(1);
 }
 
+/**
+ * Unfreezes the smart contract.
+ * 
+ * @required_auth: the contract itself
+*/
 ACTION satoshiclicker::unfreeze()
 {
     require_auth(_self);
     setFreezeFlag(0);
 }
 
+/**
+ * Updates the amount of btc for the user.
+*/
 void satoshiclicker::updatebtc(name user, string btc)
 {
     auto itr = btc_list.find(user.value);
@@ -98,6 +144,9 @@ void satoshiclicker::updatebtc(name user, string btc)
     }
 }
 
+/**
+ * Validates if the given arguments hash into the same hash send by the caller.
+*/
 void satoshiclicker::validate(name owner, string memo, string hash, string btc)
 {
     const string &value = (name{owner}.to_string() + btc + memo);
--- contracts/WAX/satoshiclicker.hpp
+++ contracts/WAX/satoshiclicker.hpp
@@ -6,100 +6,96 @@
 using namespace eosio;
 using namespace atomicassets;
 
-CONTRACT satoshiclicker : public contract {
-	public:
-		
-		using contract::contract;
+CONTRACT satoshiclicker : public contract
+{
+public:
+	using contract::contract;
 
-		satoshiclicker(name receiver, name code, datastream<const char *> ds) :
-       		// contract base class contructor
-         		contract(receiver, code, ds),
-         		// instantiate multi-index instance as data member
-         		black_list(receiver, receiver.value),
-			ref_list(receiver, receiver.value),
-			btc_list(receiver, receiver.value),
-			_frozen(receiver, receiver.value)		
-		{}
+	satoshiclicker(name receiver, name code, datastream<const char *> ds) : // contract base class contructor
+																			contract(receiver, code, ds),
+																			// instantiate multi-index instance as data member
+																			black_list(receiver, receiver.value),
+																			ref_list(receiver, receiver.value),
+																			btc_list(receiver, receiver.value),
+																			_frozen(receiver, receiver.value)
+	{
+	}
 
-		
-		TABLE user_table {
-		 	name user;
-			string btc;
-                       uint64_t primary_key() const { return user.value;}
-               };
-                
-                struct st_frozen {
-  		 	uint64_t frozen;
-		 };
-		 
-		 typedef singleton<"freeze"_n, st_frozen> tb_frozen;
-		 
+	struct st_frozen
+	{
+		uint64_t frozen;
+	};
 
-                typedef multi_index<"blacklist"_n, user_table> black_list_table;
+	typedef singleton<"freeze"_n, st_frozen> tb_frozen;	
+	
+	tb_frozen _frozen = tb_frozen(get_self(), get_self().value);
 
-                typedef multi_index<"reflist"_n, user_table> ref_list_table;
-                
-                typedef multi_index<"btclist"_n, user_table> btc_list_table;
+	TABLE user_table
+	{
+		name user;
+		string btc;
+		uint64_t primary_key() const { return user.value; }
+	};
 
-                black_list_table black_list = black_list_table(get_self(), get_self().value);
-                
-		ref_list_table ref_list = ref_list_table(get_self(), get_self().value);
-		
-		btc_list_table btc_list = btc_list_table(get_self(), get_self().value);
-		
-		tb_frozen _frozen = tb_frozen(get_self(), get_self().value);
+	typedef multi_index<"blacklist"_n, user_table> black_list_table;
 
+	typedef multi_index<"reflist"_n, user_table> ref_list_table;
 
-		ACTION ban(name user);
+	typedef multi_index<"btclist"_n, user_table> btc_list_table;
 
-		ACTION unban(name user);
-		
-		ACTION upgrade(
-				name asset_owner,
-				uint64_t asset_id,
-				ATTRIBUTE_MAP new_mutable_data,
-				string amount,
-				string memo, 
-				string hash
-				);
-				
-		ACTION mintasset(
-				name collection_name,
-        			name schema_name,
-        			int32_t template_id,
-        			name new_asset_owner,
-        			ATTRIBUTE_MAP new_mutable_data,
-        			string amount,
-        			string memo,
-        			string hash
-				);
-				
-		ACTION mintrefasset(
-				name collection_name,
-				name schema_name,
-				int32_t template_id,
-				name ref,
-				name receiver
-				);
-				
-		ACTION freeze();
-		
-		ACTION unfreeze();
-		
-		st_frozen getFreezeFlag() {
-   			st_frozen frozen_st{.frozen = 0};
-   			return _frozen.get_or_create(_self, frozen_st);
-		}
+	black_list_table black_list = black_list_table(get_self(), get_self().value);
 
-		void setFreezeFlag(const uint64_t& pFrozen) {
-  			st_frozen frozen_st = getFreezeFlag();
-  			frozen_st.frozen = pFrozen;
-  			_frozen.set(frozen_st, _self);
-		}
-				
-		void updatebtc(name user, string btc);
-		
-		void validate(name owner, string memo, string hash, string btc);
-				
-		
+	ref_list_table ref_list = ref_list_table(get_self(), get_self().value);
+
+	btc_list_table btc_list = btc_list_table(get_self(), get_self().value);
+
+	ACTION ban(name user);
+
+	ACTION unban(name user);
+
+	ACTION upgrade(
+		name asset_owner,
+		uint64_t asset_id,
+		ATTRIBUTE_MAP new_mutable_data,
+		string amount,
+		string memo,
+		string hash);
+
+	ACTION mintasset(
+		name collection_name,
+		name schema_name,
+		int32_t template_id,
+		name new_asset_owner,
+		ATTRIBUTE_MAP new_mutable_data,
+		string amount,
+		string memo,
+		string hash);
+
+	ACTION mintrefasset(
+		name collection_name,
+		name schema_name,
+		int32_t template_id,
+		name ref,
+		name receiver);
+
+	ACTION freeze();
+
+	ACTION unfreeze();
+
+	st_frozen getFreezeFlag()
+	{
+		st_frozen frozen_st{.frozen = 0};
+		return _frozen.get_or_create(_self, frozen_st);
+	}
+
+	void setFreezeFlag(const uint64_t &pFrozen)
+	{
+		st_frozen frozen_st = getFreezeFlag();
+		frozen_st.frozen = pFrozen;
+		_frozen.set(frozen_st, _self);
+	}
+
+	void updatebtc(name user, string btc);
+
+	void validate(name owner, string memo, string hash, string btc);
 };
